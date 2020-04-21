19d3490ea4288aae3d2cd63504a638a907c19a56
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-21 01:15:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,12 +69,12 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql,grade).map do |row|
       self.new_from_db(row)
     end
   end
