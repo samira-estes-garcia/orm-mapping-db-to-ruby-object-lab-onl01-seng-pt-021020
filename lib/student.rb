c465c3c890d9a13ed40466c7bdb7901dd1c637f8
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-21 01:03:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,12 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL 
-    SELECT COUNT(*) FROM students WHERE grade = 
+    SELECT COUNT(*) FROM students WHERE grade < '12'
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
